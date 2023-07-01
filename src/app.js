eac52681928a3fb8a0ba8e1f18930b123e090fa6
rafaelanobre <raafanobre@gmail.com>
2023-07-01 20:03:30 -0300

    Chore: installed MongoDB

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from "express";
 import cors from 'cors';
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+configDotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
 
 app.listen(5000);
